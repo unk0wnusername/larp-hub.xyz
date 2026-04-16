fe62b260fd09eadb26aaf74543ca7c90133899a2
unk0wnusername <163967610+unk0wnusername@users.noreply.github.com>
2026-04-16 19:21:07 -0400

    Remove unnecessary whitespace and clean up script.js

--- script.js
+++ script.js
@@ -1,41 +1,14 @@
 const card=document.getElementById("card");
-
-
-
 const enter=document.getElementById("enter");
-
-
 const audio=document.getElementById("audio");
 
-
-
-
-
 enter.onclick=()=>{
-
-
 enter.style.display="none";
-
-
 audio.play();
-
-
 };
 
-
-
-
-
 document.addEventListener("mousemove",e=>{
-
-
 const x=(e.clientX/window.innerWidth-.5)*12;
-
-
 const y=(e.clientY/window.innerHeight-.5)*-12;
-
-
 card.style.transform=`rotateX(${y}deg) rotateY(${x}deg)`;
-
-
 });
